0d157e5028db39f70152209c6d3c93380eee5c78
newh08 <newh08@gmail.com>
2023-11-24 11:51:29 +0900

    ✨FEAT: 최종 우승자 출력을 위한 DTO 생성 기능 구현

--- src/main/java/racingcar/model/CarDtoMapper.java
+++ src/main/java/racingcar/model/CarDtoMapper.java
@@ -1,10 +1,29 @@
 package racingcar.model;
 
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.model.car.Car;
+import racingcar.model.car.dto.CarDto;
+import racingcar.model.car.CarName;
+import racingcar.model.car.CarPosition;
+import racingcar.model.cars.Cars;
+import racingcar.model.cars.WinCars;
+import racingcar.model.cars.dto.WinCarDto;
+
 public class CarDtoMapper {
 
-    public static CarDto mapToCarDto(Car car) {
-        CarName carName = car.getCarName();
-        CarPosition carPosition = car.getCarPosition();
-        return new CarDto(carName.getCarName(), carPosition.getCarPosition());
+    public static List<CarDto> mapToCarDto(Cars cars) {
+        List<CarDto> carDtos = new ArrayList<>();
+        for (Car car : cars.getCars()) {
+            CarName carName = car.getCarName();
+            CarPosition carPosition = car.getCarPosition();
+            carDtos.add(new CarDto(carName.getCarName(), carPosition.getCarPosition()));
+        }
+        return carDtos;
+    }
+
+    public static WinCarDto mpaToWinCarDto(WinCars winCars) {
+        List<String> carNames = winCars.getCarNames();
+        return new WinCarDto(carNames);
     }
 }
