7535594b57a7678f813606ffcc901ff9f293253e
newh08 <newh08@gmail.com>
2023-11-24 11:52:09 +0900

    ✨FEAT: InputVIew, OutputView 기능 구현

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,21 @@
+package racingcar.view;
+
+import java.util.List;
+import racingcar.model.car.dto.CarDto;
+import racingcar.model.cars.dto.WinCarDto;
+
+public class OutputView {
+
+    public void printRaceResult(List<CarDto> carDto) {
+        for (CarDto dto : carDto) {
+            String movePath = "-".repeat(dto.getCarPosition());
+            System.out.printf("%s : %s\n", dto.getCarName(), movePath);
+        }
+        System.out.println(" ");
+    }
+
+    public void printWinner(WinCarDto winCarDto) {
+        String winnerMessage = String.join(", ", winCarDto.getCarNames());
+        System.out.printf("최종 우승자 : %s", winnerMessage);
+    }
+}
